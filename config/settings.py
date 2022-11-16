202a7767f4b79356c4b30ffbd6b20a90c7597aab
Arvinder Singh Kandola <askandola2@gmail.com>
2022-11-16 14:46:21 +0530

    Add timeout None for cache

--- config/settings.py
+++ config/settings.py
@@ -190,5 +190,6 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         # 'LOCATION': '127.0.0.1:11211',
         'LOCATION': 'memcached:11211',
+        'TIMEOUT': None,
     }
 }
