dba85b670b215c4eb7d37f3725b057561783da8a
Arvinder Singh Kandola <askandola2@gmail.com>
2022-11-24 17:16:33 +0530

    Fix complete team registration csv file generation

--- info/views.py
+++ info/views.py
@@ -123,8 +123,7 @@ def streamEventFinalRegstCSV(request, id):
             teams = event.teams.all()
             rows = [['Team ID', 'Team Name', 'User ID', 'Name', 'Email ID', 'Phone Number', 'Thapar Student', 'Roll Number', 'College Name', 'ID Proof URL', 'Fees Paid']]
             for team in teams:
-                if int(team.max_count)<int(event.min_team_size):
-                    print('done')
+                if team.members.count()+1<event.min_team_size:
                     continue
                 rows.append([team.id, team.name, team.leader.id, team.leader.name, team.leader.email, team.leader.phone_no, team.leader.is_thaparian, team.leader.roll_no, team.leader.college, team.leader.id_proof, team.amount_paid])
                 members = team.members.all()
