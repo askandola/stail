8b14f3dc6f6c685b70ff7454bf61b47d198b5a4d
Arvinder Singh Kandola <askandola2@gmail.com>
2022-12-11 00:30:21 +0530

    remove scheduled background jobs

--- registrations/apps.py
+++ registrations/apps.py
@@ -5,6 +5,6 @@ class RegistrationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'registrations'
 
-    def ready(self):
-        from jobs import sched
-        sched.schedule_job()
+    # def ready(self):
+        # from jobs import sched
+        # sched.schedule_job()
